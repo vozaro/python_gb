519cc33b0bbfb81f7637409c080dde521e46e359
vozaro <vozaro@yandex.com>
2021-02-08 01:19:20 +0300

    Заливаем ДЗ по второму уроку

--- lesson_2/dz_1.py
+++ lesson_2/dz_1.py
@@ -1,1 +1,5 @@
-print('Hello World')
+a = [123, 'Тест', 56.4, True]
+count = 0
+while count < len(a):
+    print(a[count], type(a[count]))
+    count = count + 1
--- lesson_2/dz_2.py
+++ lesson_2/dz_2.py
@@ -1,1 +1,12 @@
-print('Надо сменить')
+data = ['name', 'surname', 'age', 'city']
+
+data[0] = input('Введите ваше имя - ')
+data[1] = input('Введите вашу фамилию - ')
+data[2] = int(input('Введите ваш возраст - '))
+data[3] = input('Введите город проживания - ')
+
+print(data)
+
+data.insert(1, data.pop(0))
+data.insert(2, data.pop(3))
+print(data)
