e2e848fd73cb31e651e8755601c0dad69b7b6c3c
vozaro <78043259+vozaro@users.noreply.github.com>
2021-02-11 22:28:38 +0300

    Update task_1.py

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -1,8 +1,7 @@
 def division(divisible, divisor):
     while divisor == 0:
         divisor = (int(input('На ноль делить нельзя! Введите делитель - ')))
-    return divisible / divisor
-
-print(division(int(input('Введите делимое - ')), int(input('Введите делитель - '))))
+    print(divisible / divisor)
 
 
+division(int(input('Введите делимое - ')), int(input('Введите делитель - ')))
