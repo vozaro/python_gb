4f673f2c807e13dca0959997364eb0b2a5cc10aa
vozaro <78043259+vozaro@users.noreply.github.com>
2021-02-11 19:21:55 +0300

    Add files via upload

--- lesson_3/task_1.py
+++ lesson_3/task_1.py
@@ -1,7 +1,8 @@
 def division(divisible, divisor):
     while divisor == 0:
-        print('На ноль делить нельзя!')
-        divisor = (int(input('Введите делитель - ')))
+        divisor = (int(input('На ноль делить нельзя! Введите делитель - ')))
     return divisible / divisor
 
 print(division(int(input('Введите делимое - ')), int(input('Введите делитель - '))))
+
+
--- lesson_3/task_2.py
+++ lesson_3/task_2.py
@@ -2,10 +2,10 @@ def user_data(name, surname, birthday, city, email, tel):
     print(name, surname, birthday, city, email, tel)
 
 user_data(
-    input('Введите имя - '),
-    input('Введите фамилию - '),
-    input('Введите дату рождения - '),
-    input('Введите город проживания - '),
-    input('Введите email - '),
-    input('Введите номер телефона - '),
+    name = input('Введите имя - '),
+    surname = input('Введите фамилию - '),
+    birthday = input('Введите год рождения - '),
+    city = input('Введите город проживания - '),
+    email = input('Введите email - '),
+    tel = input('Введите номер телефона - '),
 )
