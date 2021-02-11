c5fc52ae948517ef547b640dc47ff85bc8e1e827
vozaro <78043259+vozaro@users.noreply.github.com>
2021-02-11 22:04:31 +0300

    Update task_5.py

--- lesson_3/task_5.py
+++ lesson_3/task_5.py
@@ -1,11 +1,18 @@
-def my_func(x, y):
-    while x <= 0:
-        x = float(input('Введите действительное положительное число - '))
-    while y >= 0:
-        y = int(float(input('Введите отрицательное целое число - ')))
-    for i in range(abs(y)):
-        x = x * i
-    return 1 / x
+def my_sum():
+    sum_result = 0
+    fix = False
+    while not fix:
+        number = input('Введите числа или E для выхода - ').split()
+        result = 0
+        for el in range(len(number)):
+            if number[el] == 'e' or number[el] == 'E':
+                fix = True
+                break
+            else:
+                result = result + int(number[el])
+        sum_result = sum_result + result
+        print('Текущая сумма - ', sum_result)
+    print('Финальная сумма - ', sum_result)
 
-print(my_func(float(input('Введите действительное положительное число - ')),
-                 int(float(input('Введите отрицательное целое число - ')))))
+
+my_sum()
